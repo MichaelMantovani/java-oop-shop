2b7af3f112b93cf81c7e5a599a8cfa831439e794
MichaelMantovani <michmantovani@gmail.com>
2023-11-13 15:34:30 +0100

    create Prodotto class

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,5 +1,8 @@
 package org.lessons.java.shop;
 
 public class Main {
-
+public static void main(String[] args) {
+	Prodotto miele = new Prodotto(1234,"Miele", "Molto buono", 26, 22);
+	System.out.println(miele);
+}
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,7 +1,69 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
-public static void main(String[] args) {
+
+	private int codice;
+	private String nome;
+	private String descrizione;
+	private float prezzo;
+	private float IVA;
 	
-}
+	public Prodotto(int codice, String nome, String descrizione, float prezzo, float IVA ) {
+		setCodice(codice);
+		setNome(nome);
+		setDescrizione(descrizione);
+		setPrezzo(prezzo);
+		setIVA(IVA);
+	}
+	
+	private void  setCodice(int codice) {
+		this.codice = codice;
+	}
+	
+	public int getCodice() {
+		return this.codice;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
+	public String getDescrizione() {
+		return this.descrizione;
+	}
+	
+	public void setPrezzo (float prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+	public float getPrezzo() {
+		return this.prezzo;
+	}
+	
+	public void setIVA(float IVA) {
+		this.IVA = IVA;
+	}
+	
+	public float getIVA() {
+		return this.IVA;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Prodotto:\n"
+		+ "codice: " + getCodice() + "\n"
+		+ "Nome: " + getNome() + "\n"
+		+ "descrizione: " + getDescrizione() + "\n"
+		+ "Prezzo: €" + getPrezzo() + "\n"
+		+ "IVA: " + getIVA();
+	}
 }
